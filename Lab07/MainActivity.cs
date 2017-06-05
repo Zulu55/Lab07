9b6306be1b8746f0633e966eb01ecf4e279c5bd9
Zulu <jzuluaga55@gmail.com>
2017-06-05 16:21:32 -0500

    Lab 07 Finish

---
+++ Lab07/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using SALLab07;
+
+namespace Lab07
+{
+    [Activity(Label = "Lab 07", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        EditText EmailText;
+        EditText PasswordText;
+        Button ValidateButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            EmailText = FindViewById<EditText>(Resource.Id.EmailText);
+            PasswordText = FindViewById<EditText>(Resource.Id.PasswordText);
+            ValidateButton = FindViewById<Button>(Resource.Id.ValidateButton);
+
+            ValidateButton.Click += ValidateButton_Click;
+        }
+
+        private async void ValidateButton_Click(object sender, System.EventArgs e)
+        {
+            var serviceClient = new ServiceClient();
+            var studentEmail = EmailText.Text;
+            var password = PasswordText.Text;
+            var myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            var result = await serviceClient.ValidateAsync(studentEmail, password, myDevice);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var builder = new Notification.Builder(this)
+                    .SetContentTitle("Validación de la actividad")
+                    .SetContentText($"{result.Status}\n{result.Fullname}\n{result.Token}")
+                    .SetSmallIcon(Resource.Drawable.Icon);
+                builder.SetCategory(Notification.CategoryMessage);
+                var notification = builder.Build();
+                var manager = GetSystemService(NotificationService) as NotificationManager;
+                manager.Notify(0, notification);
+            }
+            else
+            {
+                var builder = new AlertDialog.Builder(this);
+                var alert = builder.Create();
+                alert.SetTitle("Resultado de la verificación");
+                alert.SetIcon(Resource.Drawable.Icon);
+                alert.SetMessage($"{result.Status}\n{result.Fullname}\n{result.Token}");
+                alert.SetButton("Ok", (s, ev) => { });
+                alert.Show();
+            }
+        }
+    }
+}
+
